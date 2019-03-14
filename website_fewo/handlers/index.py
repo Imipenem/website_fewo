d27bdc5931ff3e80a35d63301d388be7621c2573
Imipenem <philipp_ehm@protonmail.com>
2019-03-14 22:25:13 +0100

    Implemented contact_us request validation and error handler
    - the error page will show up if any data (any field empty or email is not valid) is not valid
    
    - this is still an early implementation
    
    - should send an Email after validation and submitting the data

--- website_fewo/handlers/index.py
+++ website_fewo/handlers/index.py
@@ -1,10 +1,18 @@
 from ..app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
+from website_fewo.model.forms import validate_email
 
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        if validate_email(request.form['email']) and request.form['name'] is not None and request.form['message'] \
+                is not None:
+            return render_template('message_send.html')
+        else:
+            return render_template('message_us_error.html')
+    else:
+        return render_template('index.html')
 
 
 @app.route('/')
